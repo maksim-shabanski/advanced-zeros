9d1c590d030b74d9b6b4191d0b8ec1aa8b81c538
Maksim Shabanski <shabanskij.maksim@gmail.com>
2019-03-15 21:31:10 +0300

    feat: solution of advanced-zeros task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var primeFactors = getPrimeFactors(base);
+  var minZerosCount;
+
+  for (var prime in primeFactors) {
+    var copyNumber = number;
+    var zerosCount = 0;
+    var primeCount = primeFactors[prime];
+
+    while (copyNumber >= prime) {
+      copyNumber = Math.floor(copyNumber / prime);
+      zerosCount += copyNumber;
+    }
+
+    zerosCount = Math.floor(zerosCount / primeCount);
+    if (!minZerosCount || minZerosCount > zerosCount) {
+      minZerosCount = zerosCount;
+    }
+  }
+
+  return minZerosCount;
+};
+
+function getPrimeFactors(number) {
+  var primeNumber = 2;
+  var primeFactors = {};
+
+  while (number !== primeNumber) {
+    if (number % primeNumber === 0) {
+      number = number / primeNumber;
+      increment(primeFactors, primeNumber);
+    } else {
+      primeNumber++;
+    }
+  }
+
+  increment(primeFactors, primeNumber);
+
+  return primeFactors;
+}
+
+function increment(obj, key) {
+  var count = obj[key] || 0;
+  obj[key] = count + 1;
 }
